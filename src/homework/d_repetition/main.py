e78582dfc93bc50eedb11131a2287e943f882274
osman-jalloh-lab <osman.jalloh@g.austincc.edu>
2025-02-22 22:29:55 +0000

    Added factorial and sum odd numbers function

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,35 @@
-#
+# main.py
+
+from src.homework.d_repetition.repetition import get_factorial, sum_odd_numbers
+
+def main():
+    while True:
+        print("Homework 3 Menu")
+        print("1 - Factorial")
+        print("2 - Sum odd numbers")
+        print("3 - Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            num = int(input("Enter a number (1-9) to calculate its factorial: "))
+            while num <= 0 or num >= 10:
+                num = int(input("Please enter a number between 1 and 9: "))
+            print(f"Factorial of {num} is {get_factorial(num)}")
+
+        elif choice == "2":
+            num = int(input("Enter a number (1-99) to sum odd numbers: "))
+            while num <= 0 or num >= 100:
+                num = int(input("Please enter a number between 1 and 99: "))
+            print(f"Sum of odd numbers up to {num} is {sum_odd_numbers(num)}")
+
+        elif choice == "3":
+            exit_choice = input("Do you want to exit? (y/n): ")
+            if exit_choice.lower() == "y":
+                print("Exiting program.")
+                break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,13 @@
-#
+def get_factorial(num):
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    return factorial
+
+def sum_odd_numbers(num):
+    sum_odds = 0
+    current_num = 1
+    while current_num <= num:
+        sum_odds += current_num
+        current_num += 2  # Skip to the next odd number
+    return sum_odds
