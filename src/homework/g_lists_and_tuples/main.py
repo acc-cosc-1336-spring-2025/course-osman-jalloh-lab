0d1ed661e2e92ad1b5587553a2bb37a876e6a4bc
osman-jalloh-lab <osman.jalloh@g.austincc.edu>
2025-03-28 01:49:09 +0000

    homework 6

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,30 @@
-#
+from lists import get_lowest_list_value,get_highest_list_value
+
+def main():
+    numbers = []
+    while True:
+        print("Menu:")
+        print("1- Show the list low/high values")
+        print("2- Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            while True:
+                value = int(input("Enter a list value: "))
+                numbers.append(value)
+                another = input("Do you want to enter another value? (yes/no): ")
+                if another.lower() != 'yes':
+                    break
+            
+            lowest = get_lowest_list_value(numbers)
+            highest = get_highest_list_value(numbers)
+            print(f"The lowest value is: {lowest}")
+            print(f"The highest value is: {highest}")
+        elif choice == '2':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again!")
+
+if __name__ == '__main__':
+    main()
--- tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,5 +1,4 @@
 import unittest
-
 from src.examples.g_lists_and_tuples.lists import test_config
 
 class Test_Config(unittest.TestCase):
